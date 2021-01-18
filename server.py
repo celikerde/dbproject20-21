885560f2405af7a63d01329518af811bc07a7552
celikerde <celikerde@itu.edu.tr>
2021-01-18 19:06:08 +0300

    third

--- server.py
+++ server.py
@@ -461,7 +461,7 @@ def messages():
     return render_template('messages.html', messages = messages, sendMessageForm = sendMessageForm, updateMessageForm=updateMessageForm, sentMessages = sentMessages)
 
     @app.route('/try',methods = ["GET"])
-    def try():
+    def rty():
         return "This is a stepping to a new world!"
 
 
